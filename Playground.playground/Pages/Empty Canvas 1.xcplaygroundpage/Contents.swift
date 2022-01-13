1cf5dab07bb085764a0b8e058e5ec7ccc3a47e68
Leon Gell <lgell@lcs.on.ca>
2022-01-12 19:53:52 -0500

    final update for the day

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -48,7 +48,7 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
 canvas.defaultBorderWidth = 5
 canvas.highPerformance = false
 canvas.borderColor = Color.black
-canvas.fillColor = Color.white
+canvas.fillColor = Color(hue: 39, saturation: 90, brightness: 100, alpha: 100)
 //main face
 canvas.drawEllipse(at: Point(x: 90, y: 110), width: 80, height: 70)
 p.goToOrigin()
@@ -68,10 +68,39 @@ p.drawTo(dx: 10, dy: 9)
 p.drawTo(dx: 10, dy: -20)
 p.drawTo(dx: -20, dy: 11)
 // eyes
+canvas.drawShapesWithFill = true
+
 print(p.currentPosition())
 p.goto(dx: -100, dy: -146)
-p.goto(dx: 50, dy: 60)
+p.goto(dx: 52, dy: 60)
+
 p.drawCircle(radius: 7)
+p.fillColor = .black
+p.goto(dx: -14, dy: 0)
+p.drawCircle(radius: 7)
+p.goToOrigin()
+print(p.currentPosition())
+p.goto(dx: 90, dy: 100)
+p.turn(degrees: 135)
+p.addLine(distance: 10)
+p.turn(degrees: -135)
+p.addLine(distance: 14)
+p.turn(degrees: -135)
+p.addLine(distance: 10)
+p.turn(degrees: 45)
+p.addLine(distance: 10)
+print(p.currentPosition())
+p.turn(degrees: -90)
+p.addArc(radius: 40, angle: -20)
+p.goToOrigin()
+p.goto(dx: 90, dy: 90)
+p.turn(degrees: 20)
+print(p.currentHeading)
+p.addArc(radius: -40, angle: 20)
+p.goToOrigin()
+p.goto(dx: 60, dy: 83)
+
+
 /*:
  ## Add your code
  
