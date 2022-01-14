cb937b20d5d64f735a3e48a0cc1090b39ab420a3
Leon Gell <lgell@lcs.on.ca>
2022-01-14 17:18:08 -0500

    going dinner made good progress

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -42,17 +42,51 @@ PlaygroundPage.current.liveView = canvas
  */
 canvas.drawShapesWithFill = true
 canvas.drawShapesWithBorders = true
+canvas.highPerformance = true
+//drawer where the cats legs go over
+canvas.fillColor = Color(hue: 30, saturation: 69, brightness: 51, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 180, height: 80)
 
 
+
+
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawEllipse(at: Point(x: 70, y: 60), width: 20, height: 13)
+canvas.drawEllipse(at: Point(x: 110, y: 60), width: 20, height: 13)
+canvas.drawShapesWithBorders = true
 // Show a grid
 canvas.drawAxes(withScale: true, by: 20, color: .black)
-canvas.defaultBorderWidth = 5
-canvas.highPerformance = false
+
 canvas.borderColor = Color.black
+// fill of left leg/arm
+canvas.defaultBorderWidth = 2
+canvas.drawShapesWithFill = true
+canvas.fillColor = Color(hue: 17, saturation: 8, brightness: 52, alpha: 100)
+canvas.drawRectangle(at: Point(x: 60, y: 60), width: 20, height: 24)
+
+//fill of right leg/arm
+canvas.defaultBorderWidth = 2
+canvas.fillColor = Color(hue: 17, saturation: 8, brightness: 52, alpha: 100)
+canvas.drawRectangle(at: Point(x: 100, y: 60), width: 20, height: 24)
+
+canvas.defaultBorderWidth = 5
 
 //main face
-canvas.fillColor = Color(hue: 39, saturation: 90, brightness: 100, alpha: 100)
+canvas.fillColor = Color(hue: 17, saturation: 8, brightness: 52, alpha: 100)
 canvas.drawEllipse(at: Point(x: 90, y: 110), width: 80, height: 70)
+//white spots
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = false
+canvas.fillColor = Color.white
+canvas.drawEllipse(at: Point(x: 65, y: 70), width: 5, height: 13)
+canvas.drawEllipse(at: Point(x: 75, y: 120), width: 30, height: 35)
+canvas.drawEllipse(at: Point(x: 110, y: 140), width: 20, height: 20)
+canvas.drawEllipse(at: Point(x: 110, y: 95), width: 20, height: 10)
+
+
+canvas.drawShapesWithBorders = true
+
 //eyes
 canvas.defaultBorderWidth = 3
 canvas.fillColor = Color.white
@@ -65,24 +99,45 @@ canvas.drawEllipse(at: Point(x: 103, y: 118), width: 5, height: 5)
 canvas.drawEllipse(at: Point(x: 77, y: 118), width: 5, height: 5)
 
 //Ears
-canvas.drawShapesWithFill = false
-canvas.drawShapesWithBorders = false
-canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = true
+
 canvas.defaultBorderWidth = 6
+canvas.fillColor = Color(hue: 17, saturation: 8, brightness: 52, alpha: 100)
 canvas.drawCustomShape(with: [Point(x: 60, y: 135),Point(x: 70, y: 155),Point(x: 80, y: 146)])
 canvas.drawCustomShape(with: [Point(x: 100, y: 146),Point(x: 110, y: 155),Point(x: 120, y: 135)])
 //nose
 canvas.defaultBorderWidth = 5
 canvas.drawCustomShape(with: [Point(x: 83, y: 105),Point(x: 97, y: 105),Point(x: 90, y: 95)])
+//mouth
 canvas.lineColor = Color.black
 canvas.drawLine(from: Point(x: 90, y: 95), to: Point(x: 90, y: 90))
 canvas.drawLine(from: Point(x: 90, y: 90), to: Point(x: 80, y: 90))
 canvas.drawLine(from: Point(x: 90, y: 90), to: Point(x: 100, y: 90))
 
+//actual leg code
+canvas.defaultLineWidth = 2
+canvas.drawLine(from: Point(x: 60, y: 84), to: Point(x: 60, y: 60))
+
+canvas.drawCurve(from: Point(x: 60, y: 60), to: Point(x: 80, y: 60), control1: Point(x: 60, y: 50), control2: Point(x: 80, y: 50))
+canvas.drawLine(from: Point(x: 60, y: 60), to: Point(x: 80, y: 60))
+canvas.drawLine(from: Point(x: 80, y: 60), to: Point(x: 80, y: 74))
+canvas.drawLine(from: Point(x: 66, y: 55), to: Point(x: 66, y: 57))
+canvas.drawLine(from: Point(x: 74, y: 55), to: Point(x: 74, y: 57))
+
+canvas.drawLine(from: Point(x: 120, y: 84), to: Point(x: 120, y: 60))
+canvas.drawCurve(from: Point(x: 120, y: 60), to: Point(x: 100, y: 60), control1: Point(x: 120, y: 50), control2: Point(x: 100, y: 50))
+canvas.drawLine(from: Point(x: 120, y: 60), to: Point(x: 100, y: 60))
+canvas.drawLine(from: Point(x: 100, y: 60), to: Point(x: 100, y: 74))
+canvas.drawLine(from: Point(x: 106, y: 55), to: Point(x: 106, y: 57))
+canvas.drawLine(from: Point(x: 114, y: 55), to: Point(x: 114, y: 57))
 
 
 
 
+
+
+
+canvas.highPerformance = false
 /*:
  ## Add your code
  
